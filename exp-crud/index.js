b188db64487990808fc488141b634a4b6f8ea379
nayan97 <nayanislam085@gmail.com>
2025-05-14 22:52:09 +0600

    server is running

--- exp-crud/index.js
+++ exp-crud/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 const port = process.env.PORT || 3000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('server is runing')
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port, ${port}`);
+    
 });
