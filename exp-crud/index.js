a8c9a5dfcc171dd3894c0d01171ee1d876d6ba89
nayan97 <nayanislam085@gmail.com>
2025-05-16 15:42:45 +0600

    connect db

--- exp-crud/index.js
+++ exp-crud/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+//myMongo
+//ohcYg84Z6ZMWF4m3
+
 // middleware 
+
+const uri = "mongodb+srv://myMongo:ohcYg84Z6ZMWF4m3@cluster0.2ossgvg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
 app.use(cors());
 app.use(express.json());
 
