851a0f706fb62a5e401c1af727eecaaede05d683
nayan97 <nayanislam085@gmail.com>
2025-06-01 22:56:32 +0600

    data disply in card

--- cererhub/CareerHub-app/src/pages/Home/HotJobs.jsx
+++ cererhub/CareerHub-app/src/pages/Home/HotJobs.jsx
@@ -1,12 +1,16 @@
 import React, {use} from 'react';
+import JobsCard from '../Shared/JobsCard';
 
 const HotJobs = ({jobsPromise}) => {
     const jobs = use(jobsPromise);
     return (
-        <div>
-            {jobs.length}
+       <div className="">
+            <h2>Latest jobs</h2>
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            {jobs.map(job => <JobsCard key={job._id} job={job}></JobsCard>)}
             
         </div>
+       </div>
     );
 };
 
