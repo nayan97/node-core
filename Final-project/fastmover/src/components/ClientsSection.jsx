c19f3b9aec2abbd0797489d611029642662025fa
nayan97 <nayanislam085@gmail.com>
2025-06-22 17:50:52 +0600

    added our clients

---
+++ Final-project/fastmover/src/components/ClientsSection.jsx
@@ -0,0 +1,27 @@
+import Marquee from "react-fast-marquee";
+
+const clientLogos = [
+  "https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png",
+  "https://1000logos.net/wp-content/uploads/2021/11/Nike-Logo.png",
+  "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
+  "https://upload.wikimedia.org/wikipedia/commons/2/26/Pepsi_logo_2014.svg",
+  "https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg",
+  "https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_logo_(2012).svg"
+];
+
+const ClientsSection = () => {
+  return (
+    <div className="bg-amber-50 py-10">
+      <h2 className="text-3xl font-bold text-center mb-6">Trusted by Top Brands</h2>
+      <Marquee gradient={false} speed={50} pauseOnHover={true}>
+        {clientLogos.map((logo, idx) => (
+          <div key={idx} className="mx-8">
+            <img src={logo} alt={`Client ${idx + 1}`} className="h-12 md:h-16 object-contain grayscale hover:grayscale-0 transition duration-300" />
+          </div>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default ClientsSection;
--- Final-project/fastmover/src/pages/Home/Home.jsx
+++ Final-project/fastmover/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Services from '../../components/Services';
+import ClientsSection from '../../components/ClientsSection'
 
 const Home = () => {
     return (
         <div>
           <Banner></Banner>
           <Services></Services>
+           <ClientsSection />
         </div>
     );
 };
